15e0835e5b9017c38f4f7ab7b3ce546f357ab79d
Revant Kulkarni <75092585+revantKulkarni@users.noreply.github.com>
2023-03-20 15:47:58 +0530

    Initial commit

---
+++ src/questionnaireData.js
@@ -0,0 +1,15 @@
+export const lookupDataFoundation = () => {
+  let result = [
+    {
+      number: "1",
+      what: "Setup Pillar Operation",
+      questions: `
+      .	Pillar team is well established with roles and responsibilities documented (RACI)
+      .	Pillar Leader, deputy leader and succession plan for pillar leadership in place to ensure continuity
+      .	Gives & Gets (handshakes) with other pillars and functions defined and documented
+      .	Clear and frequent meeting drumbeat, meeting Terms of Reference documented and high meeting attendance
+`
+    }
+  ];
+  return result;
+};
